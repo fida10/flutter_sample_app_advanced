f7e6fec2dc40e8e86db31b08095cfc6e04f187f7
eshad <fidaeshad10@gmail.com>
2023-05-02 04:21:16 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main(){
+
+}
+
+class MyApp extends StatelessWidget {
+  //MyApp, the primary widget of the application
+//All widgets are here or are children of widgets that are here
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) =>
+          MyAppState(), //application global items, callable via context
+      //stuff like "theme", fonts, colors, etc. can be defined here
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+        ),
+
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  
+}
