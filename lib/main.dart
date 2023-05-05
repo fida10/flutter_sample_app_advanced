ca899ef90ea86920416bb7edf07cd4d3e343935d
shakil635 <shakiladnann635@gmail.com>
2023-05-05 23:07:56 +0600

    new comment add kora hoice shakil home work

--- lib/main.dart
+++ lib/main.dart
@@ -62,6 +62,7 @@ class MyAppState extends ChangeNotifier {
 //shakil  home work1
 
 class MyHomePage extends StatefulWidget {
+  // jode widget er modde poriborthon ba onek kisu add korrte chai tahole StatefulWidget use kori
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -71,10 +72,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    var colorScheme = Theme.of(context).colorScheme;
+    var colorScheme = Theme.of(context)
+        .colorScheme; // color proparrti babohar kora hoice colorScheme variabale er madhome (context)
 
-    Widget page;
+    Widget page; //
     switch (selectedIndex) {
+      // programing languages e 0 theke count hoy for example:0,1,2,
       case 0:
         page = GeneratorPage();
         break;
@@ -82,12 +85,12 @@ class _MyHomePageState extends State<MyHomePage> {
         page = Placeholder();
         break;
       default:
-        throw UnimplementedError('no widget for $selectedIndex');
+        throw UnimplementedError(
+            'no widget for $selectedIndex'); // $ use kora hoy string er modde kono variabale er karjoksrta ba kaj k show koranor jonno
     }
 
-    // The container for the current page, with its background color
-    // and subtle switching animation.
     var mainArea = ColoredBox(
+      // bujinai??????
       color: colorScheme.surfaceVariant,
       child: AnimatedSwitcher(
         duration: Duration(milliseconds: 200),
@@ -96,29 +99,40 @@ class _MyHomePageState extends State<MyHomePage> {
     );
 
     return Scaffold(
+      // akta building er kaj korte jamo bas ba lohar kathamo dorkar hoy app er modde scaffold ta mon kathamor moto kaj kore
       body: LayoutBuilder(
+        // simana nirdharon kora jonno,
         builder: (context, constraints) {
+          // buji nai ?
           if (constraints.maxWidth < 450) {
-            // Use a more mobile-friendly layout with BottomNavigationBar
-            // on narrow screens.
             return Column(
+              //aktar niche r akta rakhar jonno ba sajanor jono
               children: [
-                Expanded(child: mainArea),
+                Expanded(
+                    child:
+                        mainArea), // ak onsho bade baki onsho onno k deyadeyo
                 SafeArea(
+                  //app er bam side e akta jaygha creat kora j khan e bivinoo icon butto add kora hoy
                   child: BottomNavigationBar(
+                    // kono Widget er niche/bottom e button creat korar jonnoo
                     items: [
                       BottomNavigationBarItem(
-                        icon: Icon(Icons.home),
-                        label: 'Home',
+                        icon: Icon(Icons
+                            .home), // button er icon ki logo hobe ta nirdharon kora hoy
+                        label:
+                            'Home', //icon tar ki nam deya hobe ta nirdaron kora hoy
                       ),
                       BottomNavigationBarItem(
                         icon: Icon(Icons.favorite),
                         label: 'Favorites',
                       ),
                     ],
-                    currentIndex: selectedIndex,
+                    currentIndex:
+                        selectedIndex, //  icon/button k cromick vabe sajanor jonno
                     onTap: (value) {
+                      //ontap buji nai????????
                       setState(() {
+                        //145
                         selectedIndex = value;
                       });
                     },
@@ -127,11 +141,18 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             );
           } else {
+            // else babohar er karon airow er modde obastan sob button,icon side e r bottom e both show korbe
+
             return Row(
+              //widget er modde kono kisu bam theke dan e sajate babohar kora hoy
               children: [
+                //
                 SafeArea(
+                  // 107
                   child: NavigationRail(
-                    extended: constraints.maxWidth >= 600,
+                    // ati akti widget jar modde onek properti babohar kora jay backgroundColor,labelType,minWidth,elevation
+                    extended: constraints.maxWidth >=
+                        600, //layout builder er modde simana nir dharon kora hoice jode 600 ba tar soman hoy tahole safeare modde icon nam show korabe onno thay na
                     destinations: [
                       NavigationRailDestination(
                         icon: Icon(Icons.home),
@@ -145,6 +166,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     selectedIndex: selectedIndex,
                     onDestinationSelected: (value) {
                       setState(() {
+                        // safeare er modde j kono icon/button e press korle 77 number line k call back korbe than sei press e ki ghotona ghotbe ta nirdharon korbe
                         selectedIndex = value;
                       });
                     },
@@ -163,12 +185,16 @@ class _MyHomePageState extends State<MyHomePage> {
 // shakil home work2
 
 class GeneratorPage extends StatelessWidget {
+  //Stateless widget ja nirdisto /const kore rakhar jonno
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
+    var appState = context.watch<
+        MyAppState>(); // Widget er moddr kono rokom poriborton ba change hole ta dekhe Myappstate k jananor jonno watch method use koora hoy
+    var pair = appState
+        .current; //  pair variabale er modde appState.current k store kora hoice
 
-    IconData icon;
+    IconData
+        icon; // icon data ja if else statement er madhome present kora hoice if/jode (posondo hoy) heart icon show  korabe else/onnothay jode posondo na hoy tahoke heart icon show korabe na faka thake sudhu  heart icon boder ba ager ta thakbe
     if (appState.favorites.contains(pair)) {
       icon = Icons.favorite;
     } else {
@@ -177,30 +203,39 @@ class GeneratorPage extends StatelessWidget {
 
     return Center(
         child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+      //
+      mainAxisAlignment:
+          MainAxisAlignment.center, // maje borabor kono elements dekhanor jonno
       children: [
         SizedBox(height: 10),
         SizedBox(height: 10),
         Row(
-          mainAxisSize: MainAxisSize.min,
+          //
+          mainAxisSize: MainAxisSize
+              .min, // e ti row er modde thka elements gula k (min/max)side ba maje prodorshon kore
           children: [
             ElevatedButton.icon(
               onPressed: () {
+                // button er press korle ki hobe ta dekhano hoice
                 appState.toggleFavorite();
               },
               icon: Icon(icon),
               label: Text('Like'),
             ),
-            SizedBox(width: 10),
+            SizedBox(
+                width:
+                    10), // duita button er modde space barano ba komano er jonno
             ElevatedButton(
               onPressed: () {
-                appState.getNext();
+                appState.getNext(); //188
               },
               child: Text('Next'),
             ),
           ],
         ),
         Spacer(flex: 2),
+
+        ///?? bujinai
       ],
     ));
   }
