f53d7301624cc7bf98ac351dc38cc7eb837c0b21
shakil635 <shakiladnann635@gmail.com>
2023-05-06 16:47:06 +0600

    home work submint

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
